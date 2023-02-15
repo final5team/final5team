403bb68dd16c795bcc19124b00ee66f6f3a61ee3
thddudrb79 <thddudrb79@naver.com>
2023-02-15 20:30:25 +0900

    distributecontroller 삭제 usertestcontroller삭제 usertestanddistributecontroller추가 공지사항 작성,목록,상세,수정 jsp 추가 icommondao 메소드 추가 common.xml 수정 usertester.jsp 단계 변경 아이콘 추가 distributor.jsp 추가

--- src/main/java/com/oti/srm/service/DistributeService.java
+++ src/main/java/com/oti/srm/service/DistributeService.java
@@ -1,9 +1,53 @@
 package com.oti.srm.service;
 
-import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oti.srm.dao.ICommonDao;
+import com.oti.srm.dto.Request;
+import com.oti.srm.dto.RequestProcess;
+import com.oti.srm.dto.StatusHistory;
 
 @Service
-public class DistributeService implements IDistributeService{
+public class DistributeService implements IDistributeService {
+	@Autowired
+	ICommonDao commonDao;
+
+	@Override
+	@Transactional
+	public List<StatusHistory> getDevToTesterHistories(int rno) {
+		// 요청에 딸린 전체 단계 이력 가져오기
+		List<StatusHistory> requestHistories = commonDao.getRequestHistories(rno);
+
+		List<StatusHistory> devToTesterHistories = new ArrayList<StatusHistory>();
+
+		for (StatusHistory sh : requestHistories) {
+			// 개발자 -> 테스터 단계 이력만 담기
+			if (sh.getNextStatus() == 5) {
+				sh.setFileList(commonDao.getStatusHistoryFiles(sh.getHno()));
+				devToTesterHistories.add(sh);
+			}
+		}
+
+		return devToTesterHistories;
+	}
+
+	@Override
+	@Transactional
+	public Request getRequest(int rno) {
+		Request request = commonDao.getRequest(rno);
+		request.setFiles(commonDao.getRequestFiles(rno));
+		return request;
+	}
+
+	@Override
+	public RequestProcess getRequestProcess(int rno) {
+		RequestProcess requestProcess = commonDao.getRequestProcess(rno);
+		return requestProcess;
+	}
 
 }
