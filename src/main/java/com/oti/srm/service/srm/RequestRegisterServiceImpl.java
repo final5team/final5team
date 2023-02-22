7f23d6507ddaae6693d9b1b40aaf74778e2c3b5c
InRedwave <jisung1202@Naver.com>
2023-02-23 01:43:00 +0900

    #43 FILTER

--- src/main/java/com/oti/srm/service/srm/RequestRegisterServiceImpl.java
+++ src/main/java/com/oti/srm/service/srm/RequestRegisterServiceImpl.java
@@ -99,17 +99,13 @@ public class RequestRegisterServiceImpl implements IRequestRegisterService {
 	// 리스트 조회
 	@Override
 	public List<SelectPM> getPmRequestList(Request request, ListFilter listFilter, Pager pager) {
-		
-		
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("request", request);
-		map.put("listFilter", dateFilterList(listFilter));
+		map.put("listFilter", statusFilterList(dateFilterList(listFilter)));
 		map.put("pager", pager);
 		
 		log.info(listFilter.toString());
 		
-		
-//		List<SelectPM> result = requestDao.selectPmRequestList(request, dateFilterList(listFilter), pager);
 		List<SelectPM> result = requestDao.selectPmRequestList(map);
 		
 		
@@ -123,39 +119,8 @@ public class RequestRegisterServiceImpl implements IRequestRegisterService {
 		int rows = requestDao.countWorkerList(workerSno);
 		return rows;
 	}
-	
-	
-	
-	
-	
-	//날짜 필터링 메소드
-	public Request dateFilter(ListFilter listFilter, Request request) {
-		// 날짜 필터 조건 - 지정 안한 경우
-		if (listFilter.getDateFirst().isEmpty() && listFilter.getDateLast().isEmpty()) {
-			request.setDateValue("zero");
-			listFilter.setDateValue("zero");
 
-			// 날짜 필터 조건 - 시작 날짜만 지정한 경우
-		} else if (listFilter.getDateFirst().isEmpty()) {
-			request.setDateValue("first");
-			request.setDateLast(listFilter.getDateLast());
-			listFilter.setDateValue("first");
 
-			// 날짜 필터 조건 - 종료 날짜만 지정한 경우
-		} else if (listFilter.getDateLast().isEmpty()) {
-			request.setDateValue("last");
-			request.setDateFirst(listFilter.getDateFirst());
-			listFilter.setDateValue("last");
-
-			// 날짜 필터 조건 - 모두 지정한 경우
-		} else {
-			request.setDateValue("both");
-			request.setDateFirst(listFilter.getDateFirst());
-			request.setDateLast(listFilter.getDateLast());
-			listFilter.setDateValue("both");
-		}
-		return request;
-	}
 	//날짜 필터링 메소드
 	public ListFilter dateFilterList(ListFilter listFilter) {
 		// 날짜 필터 조건 - 지정 안한 경우
@@ -173,4 +138,33 @@ public class RequestRegisterServiceImpl implements IRequestRegisterService {
 		}
 		return listFilter;
 	}
+	//단계 필터링 메소드
+	public ListFilter statusFilterList(ListFilter listFilter) {
+			//1 접수
+		if(listFilter.getStatusNo() == 1) {
+			listFilter.setStatusValue("접수");
+			//2, 3, 4 개발단계
+		} else if(listFilter.getStatusNo()  == 2 || listFilter.getStatusNo()  ==3 || listFilter.getStatusNo() == 4) {
+			listFilter.setStatusValue("개발");
+			//5, 6, 7 테스트 단계
+		} else if (listFilter.getStatusNo()  == 5 || listFilter.getStatusNo()  ==6 || listFilter.getStatusNo() == 7) {
+			listFilter.setStatusValue("테스트");
+			//8 유저 테스트 단계
+		} else if(listFilter.getStatusNo()  == 8) {
+			listFilter.setStatusValue("유저테스트");
+			//10 배포 단계
+		} else if(listFilter.getStatusNo()  == 10) {
+			listFilter.setStatusValue("배포");
+			//11, 13 완료단계
+		} else if(listFilter.getStatusNo()  == 11 || listFilter.getStatusNo()  == 13) {
+			listFilter.setStatusValue("완료");
+			//12 반려
+		} else if(listFilter.getStatusNo() == 12) {
+			listFilter.setStatusValue("반려");
+		}
+		
+		return listFilter;
+	}
+	
+	
 }
