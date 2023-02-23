ed0d47c826513de423691bb82eb850180a96eccf
LemonGlacier <hijy02122@gmail.com>
2023-02-23 19:53:14 +0900

    [#8] stats - complete rate per task

--- src/main/java/com/oti/srm/controller/srm/StatsController.java
+++ src/main/java/com/oti/srm/controller/srm/StatsController.java
@@ -36,7 +36,7 @@ public class StatsController {
 		// 전체 완료율 구하기
 		model.addAttribute("comRate", statsService.getComRate());
 		// 태스크별 완료율 구하기
-		//model.addAttribute("comRateTask", statsService.getComRate(0));
+		model.addAttribute("comRateTask", statsService.getComRateTask());
 		
 		// 전체 지연율 구하기
 		model.addAttribute("delRate", statsService.getDelRate());
