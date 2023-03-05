499450eced09cd245a6d2fa3853e401b0d515fd9
jh4103 <jangh0430@gmail.com>
2023-03-05 18:49:46 +0900

    receipt.jsp 접수 및 반려에 에디터 적용 완료

--- src/main/webapp/resources/js/tinymceinit.js
+++ src/main/webapp/resources/js/tinymceinit.js
@@ -18,15 +18,18 @@ tinymce.init({
 
 tinymce.init({
 	language: 'ko_KR',
-	selector: '#document_detail',
-	height: '297mm',
+	selector: 'textarea.pmcontent',
+	height: '70mm',
 	theme: 'silver',
+	menubar: false,
 	plugins: [
-		'noneditable'
-		],
-	menubar : '',
-	toolbar1: 'fullscreen print preview wordcount',
-	readonly: 1,
-	content_css : '../resources/css/approvalform.css',
-	object_resizing : false
+	    'print noneditable save',
+	    'wordcount fullscreen charmap hr textcolor colorpicker textpattern',
+	    'template searchreplace insertdatetime pagebreak emoticons'
+	],
+	toolbar2: 'undo redo | insert | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent',
+	template_mdate_format: '%m/%d/%Y : %H:%M',
+	save_enablewhendirty: false,
+	object_resizing : false,
+	forced_root_block : ""
 });
