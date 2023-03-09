4a629814075d150d944a6aebf403ddea457fb0af
thddudrb79 <thddudrb79@naver.com>
2023-03-09 14:14:13 +0900

    pmcontroller, service, dao 수정

--- src/main/java/com/oti/srm/controller/srm/PMController.java
+++ src/main/java/com/oti/srm/controller/srm/PMController.java
@@ -35,7 +35,7 @@ public class PMController {
 	ICommonService commonService;
 	@Autowired
 	IPMService pMService;
-	
+
 	/**
 	 * 
 	 * @author: KIM JI YOUNG
@@ -48,9 +48,9 @@ public class PMController {
 	@GetMapping("/receiptdetail")
 	public String receiptDetail(int rno, HttpSession session, Model model) {
 		// 요청정보
-		Request request=commonService.getRequest(rno);
+		Request request = commonService.getRequest(rno);
 		model.addAttribute("request", request);
-		// 요청 상태가 접수중일 때  담당자 정보
+		// 요청 상태가 접수중일 때 담당자 정보
 		// 개발자 정보
 		model.addAttribute("devStaffList", pMService.getStaffBySno(request.getSno(), "developer"));
 		// 테스터 정보
@@ -60,21 +60,21 @@ public class PMController {
 		// 배포자 정보
 		model.addAttribute("disStaffList", pMService.getStaffBySno(request.getSno(), "distributor"));
 		model.addAttribute("requestProcess", commonService.getRequestProcess(rno));
-		model.addAttribute("pmToAllHistories", commonService.getPmToAllHistories(rno));	
+		model.addAttribute("pmToAllHistories", commonService.getPmToAllHistories(rno));
 		model.addAttribute("testRejectExist", commonService.isThereTestReject(rno));
-		// 확인 여부 
-		if(request.getPmCheck()==1) {
+		// 확인 여부
+		if (request.getPmCheck() == 1) {
 			// PM 확인 여부 변경
 			commonService.check("pm", request.getRno());
-		}		
+		}
 		// 요청 상태가 반려일 때 상태 변경 정보(반려 사유)
-		if(request.getStatusNo()==12) {
+		if (request.getStatusNo() == 12) {
 			model.addAttribute("rejectHistory", pMService.getStatusHistory(rno, "reject"));
 		}
-			
+
 		return "srm/receipt";
 	}
-	
+
 	/**
 	 * 
 	 * @author: KIM JI YOUNG
@@ -86,21 +86,22 @@ public class PMController {
 	 * @return
 	 */
 	// 접수하기
-	@RequestMapping(value="/receipt", method = RequestMethod.POST)
-	public String receipt(StatusHistory statusHistory, RequestProcess requestProcess, MultipartFile[] files, HttpSession session, Model model) {
+	@RequestMapping(value = "/receipt", method = RequestMethod.POST)
+	public String receipt(StatusHistory statusHistory, RequestProcess requestProcess, MultipartFile[] files,
+			HttpSession session, Model model) {
 		try {
 			// 작성자 입력
-			Member me = (Member) session.getAttribute("member");		
+			Member me = (Member) session.getAttribute("member");
 			statusHistory.setWriter(me.getMid());
 			requestProcess.setPm(me.getMid());
-			
+
 			// 첨부파일 매핑
-			if(files!=null) {
+			if (files != null) {
 				// 첨부파일 저장할 List 객체 생성
-				List<StatusHistoryFile> fileList=new ArrayList<>();
-				for(MultipartFile file : files) {
+				List<StatusHistoryFile> fileList = new ArrayList<>();
+				for (MultipartFile file : files) {
 					// 첨부파일 값이 존재할 때
-					if(file!=null && !file.isEmpty()) {
+					if (file != null && !file.isEmpty()) {
 						// 상태 이력 파일 DTO에 저장
 						StatusHistoryFile shf = new StatusHistoryFile();
 						// 첨부파일 이름 저장
@@ -115,19 +116,19 @@ public class PMController {
 				}
 				// 상태 이력 기록 파일에 파일 저장
 				statusHistory.setFileList(fileList);
-			}			
-			// 접수 완료
-			int result=pMService.receipt(statusHistory, requestProcess);
-			// 접수 성공
-			if(result==1) {
-				return "redirect:/customer/requestlist"; 
 			}
-		} catch(Exception e) {
+			// 접수 완료
+			int result = pMService.receipt(statusHistory, requestProcess);
+			// 접수 성공
+			if (result == 1) {
+				return "redirect:/customer/requestlist";
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-		}		
-		return "redirect:/pm/receiptdetail?rno="+requestProcess.getRno();
+		}
+		return "redirect:/pm/receiptdetail?rno=" + requestProcess.getRno();
 	}
-	
+
 	/**
 	 * 
 	 * @author: KIM JI YOUNG
@@ -140,13 +141,13 @@ public class PMController {
 	@GetMapping("/completedetail")
 	public String completeDetail(int rno, HttpSession session, Model model) {
 		// 서비스 요청 정보
-		model.addAttribute("request", commonService.getRequest(rno));	
+		model.addAttribute("request", commonService.getRequest(rno));
 		// 요청 처리 정보
 		model.addAttribute("reqProcess", commonService.getRequestProcess(rno));
-		
+
 		return "srm/complete";
 	}
-	
+
 	/**
 	 * 
 	 * @author: KIM JI YOUNG
@@ -157,21 +158,21 @@ public class PMController {
 	 */
 	// 완료 처리하기
 	@RequestMapping("/complete")
-	public String complete(StatusHistory statusHistory, HttpSession session, Model model) {	
+	public String complete(StatusHistory statusHistory, HttpSession session, Model model) {
 		// 작성자 입력
-		Member me = (Member) session.getAttribute("member");		
+		Member me = (Member) session.getAttribute("member");
 		statusHistory.setWriter(me.getMid());
-		
-		//최종 완료 처리
+
+		// 최종 완료 처리
 		statusHistory.setNextStatus(13);
 		// PM 처리 완료(완료 승인)
 		commonService.endWork(statusHistory, me.getMtype());
 		// 서비스 변경 여부(사용자 미확인)
 		commonService.notCheck("user", statusHistory.getRno());
-		
+
 		return "redirect:/pm/enddetail?rno=" + statusHistory.getRno();
 	}
-	
+
 	/**
 	 * 
 	 * @author: KIM JI YOUNG
@@ -185,12 +186,12 @@ public class PMController {
 	public String endDetail(int rno, HttpSession session, Model model) {
 		// 서비스 요청 정보
 		Request request = commonService.getRequest(rno);
-		model.addAttribute("request", request);	
+		model.addAttribute("request", request);
 		// 요청 처리 정보
 		model.addAttribute("requestProcess", commonService.getRequestProcess(rno));
 		// 요청 처리 내역
 		// 요청 상태가 반려일 때 상태 변경 정보(반려 사유)
-		if(request.getStatusNo()==12) {
+		if (request.getStatusNo() == 12) {
 			model.addAttribute("rejectHistory", pMService.getStatusHistory(rno, "reject"));
 		}
 		model.addAttribute("devStatusHistory", pMService.getStatusHistory(rno, "developer"));
@@ -198,10 +199,27 @@ public class PMController {
 		model.addAttribute("uteStatusHistory", pMService.getStatusHistory(rno, "usertester"));
 		model.addAttribute("disStatusHistory", pMService.getStatusHistory(rno, "distributor"));
 		model.addAttribute("testRejectExist", commonService.isThereTestReject(rno));
-		
+
 		return "srm/end";
-	}	
-	
-	//화면에서 입력한 날짜 컨트롤러의 Date 매개변수에 넣기
-	//@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date expectDate
+	}
+
+	// 화면에서 입력한 날짜 컨트롤러의 Date 매개변수에 넣기
+	// @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date expectDate
+
+	// 완료 처리하기
+	@GetMapping("/workinginfo")
+	public String workinginfo(StatusHistory statusHistory, HttpSession session, Model model) {
+		// 작성자 입력
+		Member me = (Member) session.getAttribute("member");
+		statusHistory.setWriter(me.getMid());
+
+		// 최종 완료 처리
+		statusHistory.setNextStatus(13);
+		// PM 처리 완료(완료 승인)
+		commonService.endWork(statusHistory, me.getMtype());
+		// 서비스 변경 여부(사용자 미확인)
+		commonService.notCheck("user", statusHistory.getRno());
+
+		return "redirect:/pm/enddetail?rno=" + statusHistory.getRno();
+	}
 }
--- src/main/java/com/oti/srm/service/srm/PMService.java
+++ src/main/java/com/oti/srm/service/srm/PMService.java
@@ -1,6 +1,8 @@
 package com.oti.srm.service.srm;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,8 +12,10 @@ import com.oti.srm.dao.srm.ICommonDao;
 import com.oti.srm.dao.srm.IPMDao;
 import com.oti.srm.dto.Member;
 import com.oti.srm.dto.RequestProcess;
+import com.oti.srm.dto.Status;
 import com.oti.srm.dto.StatusHistory;
 import com.oti.srm.dto.StatusHistoryFile;
+import com.oti.srm.dto.WorkingInfo;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -26,15 +30,53 @@ public class PMService implements IPMService {
 	IPMDao pMDao;
 	@Autowired
 	ICommonDao commonDao;
-	
+
 	@Override
 	@Transactional
 	public List<Member> getStaffBySno(int sno, String mtype) {
 		// 태스크별 목록 구하기
 		List<Member> staffList = pMDao.selectStaffBySno(sno, mtype);
 		// 현재 담당 건수 구하기
-		for(Member staff:staffList) {
-			staff.setQuota(pMDao.selectQuota(staff.getMid(),staff.getMtype()));
+		for (Member staff : staffList) {
+			List<Status> statusList = commonDao.selectMyWorkStatus(staff);
+			HashMap<String, Integer> map = new HashMap<>();
+			map.put("대기", 0);
+			map.put("진행중", 0);
+			for (Status status : statusList) {
+				if (staff.getMtype().equals("developer")) {
+					if (status.getStatusNo() < 4) {
+						map.put("대기", map.get("대기") + status.getCount());
+					}
+					else if (status.getStatusNo() == 4) {
+						map.put("진행중", map.get("진행중") + status.getCount());
+					}
+				}
+				else if (staff.getMtype().equals("tester")) {
+					if (status.getStatusNo() < 6) {
+						map.put("대기", map.get("대기") + status.getCount());
+					}
+					else if (status.getStatusNo() == 6) {
+						map.put("진행중", map.get("진행중") + status.getCount());
+					}
+				}
+				else if (staff.getMtype().equals("usertester")) {
+					if (status.getStatusNo() < 8) {
+						map.put("대기", map.get("대기") + status.getCount());
+					}
+					else if (status.getStatusNo() == 8) {
+						map.put("진행중", map.get("진행중") + status.getCount());
+					}
+				}
+				else if (staff.getMtype().equals("distributor")) {
+					if (status.getStatusNo() < 10) {
+						map.put("대기", map.get("대기") + status.getCount());
+					}
+					else if (status.getStatusNo() == 10) {
+						map.put("진행중", map.get("진행중") + status.getCount());
+					}
+				}
+			}
+			staff.setQuota(map);
 		}
 		return staffList;
 	}
@@ -44,34 +86,34 @@ public class PMService implements IPMService {
 	public int receipt(StatusHistory statusHistory, RequestProcess requestProcess) {
 		try {
 			// 서비스 요청->접수 완료
-			commonDao.updateRequestStatus(statusHistory.getRno(), statusHistory.getNextStatus());	
+			commonDao.updateRequestStatus(statusHistory.getRno(), statusHistory.getNextStatus());
 			// 접수 완료 이력 작성
 			commonDao.insertStatusHistory(statusHistory);
 			// 첨부 파일 등록
-			List<StatusHistoryFile> fileList =statusHistory.getFileList();
-			if(fileList !=null) {
-				for(StatusHistoryFile file: fileList) {
+			List<StatusHistoryFile> fileList = statusHistory.getFileList();
+			if (fileList != null) {
+				for (StatusHistoryFile file : fileList) {
 					file.setHno(statusHistory.getHno());
 					commonDao.insertStatusHistoryFile(file);
 				}
-			}			
+			}
 			// 서비스 요청 처리 프로세스
 			// 접수 완료
-			if(statusHistory.getNextStatus()==2) {
+			if (statusHistory.getNextStatus() == 2) {
 				// 서비스 처리 내역 생성
 				int result = pMDao.insertRequestProcess(requestProcess);
 				// 서비스 변경 여부(개발자 미확인 상태 변경)
 				result = commonDao.updateNotCheck("developer", statusHistory.getRno());
-				return (result==1)?1:0;	
-			//반려 처리	
-			} else if(statusHistory.getNextStatus()==12){
+				return (result == 1) ? 1 : 0;
+				// 반려 처리
+			} else if (statusHistory.getNextStatus() == 12) {
 				// 서비스 변경 여부(사용자 미확인 상태 변경)
 				return commonDao.updateNotCheck("user", statusHistory.getRno());
-			}			
-		} catch(Exception e) {
-			e.printStackTrace();			
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		return 0;		
+		return 0;
 	}
 
 	@Override
@@ -81,14 +123,20 @@ public class PMService implements IPMService {
 		// 전체 요청 처리 내역
 		List<StatusHistory> histories = commonDao.selectRequestHistories(rno);
 		// 처리 내역 확인
-		for(StatusHistory sh : histories) {
-			if(sh.getNextStatus() == 12) {
+		for (StatusHistory sh : histories) {
+			if (sh.getNextStatus() == 12) {
 				sh.setFileList(commonDao.selectStatusHistoryFiles(sh.getHno()));
 				reject = sh;
 				break;
 			}
 		}
 		return reject;
+	}
+
+	@Override
+	public List<WorkingInfo> getWorkingInfo(Member member) {
+		return pMDao.selectWorkingInfo(member);
+		
 	}
 
 }
