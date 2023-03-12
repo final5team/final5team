890b2ff58eaa12653ad38343b4960b45f87486b0
InRedwave <jisung1202@Naver.com>
2023-03-12 18:48:58 +0900

    #43 아이디 중복검사 수정

--- src/main/java/com/oti/srm/controller/member/CheckController.java
+++ src/main/java/com/oti/srm/controller/member/CheckController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oti.srm.dto.Member;
 import com.oti.srm.service.member.IMemberService;
 
 import lombok.extern.log4j.Log4j2;
@@ -20,9 +24,14 @@ public class CheckController {
 	//아이디 확인 ajax
 	@PostMapping("/idconfrim")
 	@ResponseBody
-	public int idConfirm(@RequestBody String mid) {
+	public int idConfirm(@RequestBody String mid) throws JsonMappingException, JsonProcessingException {
 		log.info(mid);
-		int result = memberService.checkId(mid);
+		ObjectMapper objectMapper = new ObjectMapper();
+		Member member = objectMapper.readValue(mid, Member.class);
+		
+		log.info(member.getMid());
+		int result = memberService.checkId(member.getMid());
+		
 		return result;
 	}
 	
