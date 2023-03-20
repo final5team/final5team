7f70e282cc5e66b436b1f3c4231b7ca7e9c1b82a
InRedwave <jisung1202@Naver.com>
2023-03-20 09:25:24 +0900

    #43 pull

--- src/main/java/com/oti/srm/controller/srm/RequestController.java
+++ src/main/java/com/oti/srm/controller/srm/RequestController.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -444,6 +445,7 @@ public class RequestController {
 	
 	//요청 글 상세보기
 	@GetMapping("/requestdetail")
+	@Transactional
 	public String userRequestDetail(int rno, HttpSession session, Model model) {
 		Request request = requestService.getRequestDetail(rno);
 		List<System> systemList = userRegisterService.getSystemList();
